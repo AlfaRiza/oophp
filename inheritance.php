b3c32d661614a1e87c2a0bc3c632bb1b3aa84302
AlfaRiza <malfariza45@gmail.com>
2019-06-25 23:28:19 +0700

    penerapan inheritance

--- inheritance.php
+++ inheritance.php
@@ -10,24 +10,22 @@
                 $penerbit,
                 $harga,
                 $jmlHalaman,
-                $waktuMain,
-                $tipe;
+                $waktuMain;
         
-        public function __construct($judul, $penulis, $penerbit, $harga, $jmlHalaman, $waktuMain, $tipe){
+        public function __construct($judul, $penulis, $penerbit, $harga, $jmlHalaman, $waktuMain){
             $this->judul = $judul;
             $this->penulis = $penulis;
             $this->penerbit = $penerbit;
             $this->harga = $harga;
             $this->jmlHalaman = $jmlHalaman;
             $this->waktuMain = $waktuMain;
-            $this->tipe = $tipe;
         }
 
         public function getLabel(){
             return "$this->penulis,$this->penerbit";
         }
 
-        public function getInfoLengkap(){
+        public function getInfoProduk(){
             $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
             if($this->tipe == "Komik"){
                 $str.= " - {$this->jmlHalaman} Halaman.";
@@ -40,6 +38,20 @@
 
     }
 
+    class Komik extends Produk {
+        public function getInfoProduk(){
+            $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+            return $str;
+        }
+    }
+
+    class Game extends Produk {
+        public function getInfoProduk(){
+            $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->waktuMain} Jam.";
+            return $str;
+        }
+    }
+
     class CetakInfoProduk{
         public function cetak( Produk $produk ){
             $str = "{$produk->judul} | {$produk->getLabel()} ({$produk->harga})";
@@ -48,8 +60,8 @@
 
     }
 
-    $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
-    $produk2 = new Produk("Uncharter", "Downy Donke", "Atlas", 250000, 0, 50, "Game");
+    $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0);
+    $produk2 = new Game("Uncharter", "Downy Donke", "Atlas", 250000, 0, 50);
 
     echo  $produk1->getInfoLengkap();
     echo "<br>";
